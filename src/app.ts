5168eb33e6cb3a826fa17fe2a55482113b9b1125
Matt Gruskin <matthew.gruskin@gmail.com>
2019-11-21 17:38:14 -0800

    Catch Slack platform errors and log their messages

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as bodyParser from "body-parser";
 import * as express from "express";
 import { App, ExpressReceiver } from "@slack/bolt";
+import { ErrorCode, WebAPIPlatformError } from "@slack/web-api";
 
 export const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -17,4 +18,14 @@ receiver.app.use("/static", express.static("public"));
 export const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   receiver
+});
+
+app.error(e => {
+  if (e.code === ErrorCode.PlatformError) {
+    const platformError = e as WebAPIPlatformError;
+    if (platformError.data && platformError.data.response_metadata) {
+      console.error(platformError.data.response_metadata.messages);
+    }
+  }
+  throw e;
 });
